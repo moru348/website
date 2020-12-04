cf78a39899ade5253fd7a63b0eee6caa16ac36ed
moru3 <moru348@gmail.com>
2020-12-04 17:35:29 +0900

    update

---
+++ deleteIndex.js
@@ -0,0 +1,3 @@
+if(location.href.endsWith("/index.html")) {
+    location.href = location.href.replace("/index.html", "");
+}
--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-Swal.fire({
-    icon: 'info',
-    title: "This site is incomplete.",
-    text: "Please note that there may be something wrong with it."
+if(localStorage.getItem("Notify") !== "incomplete-1") {
+    Swal.fire({
+        icon: 'info',
+        title: "This site is incomplete.",
+        text: "Please note that there may be something wrong with it."
+    });
+    localStorage.setItem("Notify", "incomplete-1");
+}
+
+let title = document.getElementById("title");
+const titles = ["I",
+"Ia",
+"Iam",
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>()</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>()</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'m\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'mo\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'mor\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="margin-left: 2px">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="margin-left: 2px">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="margin-left: 2px">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="margin-left: 2px">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px;">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px;">4\'</span>)</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px;">4\'</span>)</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px;">4\'</span>)</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px;">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="border-left: solid 2px #4bb2f76e;">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="border-left: solid 2px #4bb2f76e;">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="border-left: solid 2px #4bb2f76e;">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px">4</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span class="namethree" style="margin-left: 2px">3</span>\')</span>',
+];
+
+titles.forEach((ele, index) => {
+    setTimeout(() => {
+        title.innerHTML = ele
+    }, 130 * ++index);
 })
