31967a3ec38e1b11eeadee57c48bd0a39ce9cef5
moru3 <moru348@gmail.com>
2020-12-04 18:32:01 +0900

    update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if(localStorage.getItem("Notify") !== "incomplete-1") {
 }
 
 let title = document.getElementById("title");
-let titles = ["I",
+const defaultTitles = ["I",
 "Ia",
 "Iam",
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>()</span>',
@@ -37,14 +37,27 @@ let titles = ["I",
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px">4</span>\')</span>',
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span class="namethree" style="margin-left: 2px">3</span>\')</span>',
 ];
+let titles = defaultTitles
 var ref = document.referrer;
 var sStr = "^https?://" + window.location.hostname;
 var rExp = new RegExp( sStr, "i" );
 if( ref.length != 0 ) {
     if( ref.match( rExp ) ) {
-        titles = ['<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span class="namethree" style="margin-left: 2px">3</span>\')</span>']
+        titles = ['<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span class="namethreenoanm" style="margin-left: 2px">3</span>\')</span>']
     }
 }
+var perfEntries = performance.getEntriesByType("navigation");
+perfEntries.forEach(function(pe){
+    console.log(pe)
+    switch( pe.type ){
+        case 'reload':
+            titles = defaultTitles
+            break;
+        case 'back_forward':
+            titles = ['<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span class="namethreenoanm" style="margin-left: 2px">3</span>\')</span>']
+            break;
+    }
+});
 titles.forEach((ele, index) => {
     setTimeout(() => {
         title.innerHTML = ele
