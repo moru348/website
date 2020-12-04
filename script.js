4ca50f826f2c5142ed39415bd58d74fa25d57766
moru3 <moru348@gmail.com>
2020-12-04 18:09:11 +0900

    update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if(localStorage.getItem("Notify") !== "incomplete-1") {
 }
 
 let title = document.getElementById("title");
-const titles = ["I",
+let titles = ["I",
 "Ia",
 "Iam",
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>()</span>',
@@ -20,7 +20,6 @@ const titles = ["I",
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'mo\')</span>',
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'mor\')</span>',
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru\')</span>',
-'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="margin-left: 2px">4</span>\')</span>',
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="margin-left: 2px">4</span>\')</span>',
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="margin-left: 2px">4</span>\')</span>',
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="margin-left: 2px">4</span>\')</span>',
@@ -38,7 +37,14 @@ const titles = ["I",
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px">4</span>\')</span>',
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span class="namethree" style="margin-left: 2px">3</span>\')</span>',
 ];
-
+var ref = document.referrer;
+var sStr = "^https?://" + window.location.hostname;
+var rExp = new RegExp( sStr, "i" );
+if( ref.length != 0 ) {
+    if( ref.match( rExp ) ) {
+        titles = ['<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span class="namethree" style="margin-left: 2px">3</span>\')</span>']
+    }
+}
 titles.forEach((ele, index) => {
     setTimeout(() => {
         title.innerHTML = ele
