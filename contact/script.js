61b87b157ab8cde1841b53ef2417b6f6207ca8f1
moru3 <moru348@gmail.com>
2020-12-05 23:14:53 +0900

    update

--- contact/script.js
+++ contact/script.js
@@ -1,3 +1,3 @@
-document.getElementById("discord_id").addEventListener("input", function(event) {
+document.getElementById("discord_id").addEventListener("input", function() {
     document.getElementById("discord_id").innerHTML = 'moru3<span style="font-weight: 400;color: rgb(168, 168, 168)">#3651';
 });
