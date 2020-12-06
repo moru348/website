83f37a90c709d0d5c94a6c10a4fd08234296b95c
moru3 <moru348@gmail.com>
2020-12-06 22:28:38 +0900

    update

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const defaultTitles = ["I",
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px">4</span>\')</span>',
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px">4</span>\')</span>',
 '<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span style="background: #4bb2f76e; color: white;margin-left: 2px">4</span>\')</span>',
-'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'moru<span class="namethree" style="margin-left: 2px">3</span>\')</span>',
+'<span style="color: hsl(32, 88%, 51%)">Iam</span>(\'<span class="name" id="name" style="color: hsl(0, 0%, 92%);">moru<span class="namethree" style="margin-left: 2px">3</span></span>\')</span>',
 ];
 let titles = defaultTitles
 var ref = document.referrer;
